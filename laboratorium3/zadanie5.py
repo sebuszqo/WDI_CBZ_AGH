7b25fcd6207f3400b5dc7a299f5a8e480b52cef4
sebuszqo <sekn.kn@gmail.com>
2021-11-02 13:42:25 +0100

    Comit przed pull'em

--- laboratorium3/zadanie5.py
+++ laboratorium3/zadanie5.py
@@ -1,3 +1,3 @@
 if __name__ == '__main__':
-    liczba = int(input("podaj prosze liczbe jaka mam wypisac: "))
-    print(f"Wprowadzona przez Ciebie liczba to: {liczba}")
+    number = int(input("podaj prosze liczbe jaka mam wypisac: "))
+    print(f"Wprowadzona przez Ciebie liczba to: {number}")
