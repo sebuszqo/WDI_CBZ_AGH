d2d92bb9d2780afc37f64a24f5d2dcb97d9a4235
sebuszqo <sekn.kn@gmail.com>
2021-11-09 10:27:38 +0100

    Zadanie lab5

--- laboratorium_5/l_pierwsze.py
+++ laboratorium_5/l_pierwsze.py
@@ -1,27 +1,51 @@
-def pierwszosc (x):
-    if x <= 1:
+def pierwszosc(number):
+    if number == 2:
+        return True
+    elif number % 2 == 0 or number <= 1:
         return False
-    for l in range(2,int(x**0.5)):
-        if x % l ==0:
-            return False   
+    for f in range(2, int(number**0.5)+1):
+        if number % f == 0:
+            return False
     return True
 
 while True:
-    x = int(input("Podaj liczbe jaka mam sprawdzic: "))
-    if pierwszosc(x) == False:
-        while not pierwszosc(x):
-            x +=1
-        print(f'najblizsza liczba pierwsza blisko twojej jest: {x}')
-    else: 
+    number = int(input("Podaj liczbe jaka mam sprawdzic: "))
+    print()
+    if pierwszosc(number) == False:
+        print("Twoja liczba nie jest pierwsza")
+        while pierwszosc(number)==False:
+            number += 1
+            pierwszosc(number)
+        print(f'Najblizsza liczba pierwsza blisko twojej jest: {number}')
+    else:
         print("Podana liczba jest liczba pierwasza")
 
 
+
+
+
+
+
+
+
+
+
+
+
+# number = int((input(":")))
+# pierwszosc(number)
+
+# if pierwszosc == True:
+#     print("Podana liczba jest pierwsza")
+# else:
+#     print("Podana liczba nie jest pierwsza")
+
 # b = int(input("Podaj koncowy przedzial: "))
-# for x in range(2,b):
-#     print(x, pierwszosc(x))
+# for number in range(2,b):
+#     print(number, pierwszosc(number))
 
 
-# if pierwszosc(x) == False:
+# if pierwszosc(number) == False:
 #     print("Liczba nie jest pierwsza")
 # else:
 #     print("Liczba jest pierwsza")
