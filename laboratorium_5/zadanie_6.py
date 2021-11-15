9a2a17cdb344cbfe0ee754ad27109c24c5e8ad4d
sebuszqo <sekn.kn@gmail.com>
2021-11-15 12:12:32 +0100

    commit z krk

--- laboratorium_5/zadanie_6.py
+++ laboratorium_5/zadanie_6.py
@@ -1,9 +1,9 @@
-def pierwszosc(number):
+def prime(number):
     if number == 2:
         return True
     elif number % 2 == 0 or number <= 1: #wyrzucone liczby parzyste, wiec w dalszym kroku mozna nie sprawdzac podzielnosci przez l.parzyste
         return False
-    for f in range(3, int(number**0.5) + 1,2): #przedział od 3 do number**0.5 + 1, poniewaz dzielniki wystepuja parami.
+    for f in range(3, int(number/2)+1,2): 
         if number % f == 0:
             return False
     return True
@@ -11,17 +11,23 @@ def pierwszosc(number):
 while True:
     number = int(input("Podaj liczbe jaka mam sprawdzic: "))
     print()
-    if pierwszosc(number) == False:
+    prime(number)
+    if prime(number) == False:
         print("Twoja liczba nie jest pierwsza")
-        while pierwszosc(number)==False:
+        while prime(number) == False:
             number += 1
-        print(f'Najblizsza liczba pierwsza blisko twojej jest: {number}')
+        print(f'Najblizsza większa liczba pierwsza to: {number}')
         print()
     else:
         print("Podana liczba jest liczba pierwszą")
         print()
 
 
+
+
+# b = int(input("Podaj koncowy przedzial: "))
+# for number in range(2,b):
+#     print(number, prime(number))
 
 
 
@@ -34,19 +40,15 @@ while True:
 
 
 # number = int((input(":")))
-# pierwszosc(number)
+# prime(number)
 
-# if pierwszosc == True:
+# if prime == True:
 #     print("Podana liczba jest pierwsza")
 # else:
 #     print("Podana liczba nie jest pierwsza")
 
-# b = int(input("Podaj koncowy przedzial: "))
-# for number in range(2,b):
-#     print(number, pierwszosc(number))
 
-
-# if pierwszosc(number) == False:
+# if prime(number) == False:
 #     print("Liczba nie jest pierwsza")
 # else:
 #     print("Liczba jest pierwsza")
