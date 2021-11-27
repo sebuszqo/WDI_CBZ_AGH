4cd158e56e740ea150b9792e6e142159a1da4e87
Sebuszqo <sekn.kn@gmail.com>
2021-11-27 13:44:35 +0100

    zadanie_1 lab_8

--- laboratorium_8/zadanie_1.py
+++ laboratorium_8/zadanie_1.py
@@ -1,58 +1,79 @@
 import random
 from typing import Counter
 
-def prime(a): #funkcja sprawdzajaca czy liczba jest pierwsza 
+
+def prime(a):  # funkcja sprawdzajaca czy liczba jest pierwsza
     if a == 2 or a == 3:
         return True
-    elif a % 2 == 0 or a % 3 == 0 or a <= 1: 
+    elif a % 2 == 0 or a % 3 == 0 or a <= 1:
         return False
     i = 5
     while i*i < a:
         if a % i == 0 or a % (i+2) == 0:
-            return False 
+            return False
         i += 6
     return True
 
-def problem(tab): # glowna funkcja dopisujaca oraz sumujaca
-        long = len(tab)
-        counter = 0
-        for i in range(long): # pierwsza petla sprawdzajaca ilosc liczb pierwszych w tablicy
-            if prime(tab[i]):
-                counter += 1 
-        long += counter
-        for i in range(long): # druga petla dodajaca 0 po kazdej liczbie pierwszej
-            a = tab[i]
-            if prime(a):
-                tab.insert(i + 1, 0) # moduł .insert (miejsce w tablicy, wartosc) dodaje 0
-            else:
-                continue
-        tab2 = [] # druga tablica ktora ma przechowywac sumy podzbiorów
-        sum = 0   
-        for s in range(long):
-            if tab[s] != 0: # warunek na wykrycie 0 w tablicy
-                sum += tab[s]
-            else:
-                tab2.append(sum) # jesli nie bedzie 0 to dopisze wczesniej policzona sume do nowej tablicy 
-                sum = 0 # wyzerowanie "licznika" sumy aby mozna bylo liczyc kolejny podzbior
-        return tab,  tab2       
 
-while True:            
-    n = int(input("Podaj ile liczb mam wpisac w tablice: "))
-    tab = [random.randint(1, 100) for i in range(n)]
-    # tab = [7,45,45,34,53,45,4,3,11,18]
-    # tab =[77, 53, 92]
-    returnik = problem(tab)
-    print(returnik)
+def nwd(first, second):  # funkcja szukajaca NWD z dwoch liczb
+    if second == 0:
+        return first
+    else:
+        r = first % second
+        return nwd(second, r)
 
 
-##### Przypadki testowe #####
-# Przypadek nr 1:
-# output: lista po dodaniu 0
-# Przypadek nr 2:
-# output: lista po dodaniu 0 [7,0,45,45,34,53,0,45,4,3,0,11,0,18,30]. Wynik: 7;177;52,11.
-# Przypadek nr 3:
-# output: lista po dodaniu 0
-# Przypadki kolejne: 
-# Program jako głowne zalozenie ma generowac liczby losowe i wpisywac je do tablicy, tak wiec reszta sprawdzen moze zostac wykonana na losowo dobranych tabliach: 
-# Przypadek z problemem: 
-# gdy n =1000000, progam ma problem.
+def problem(tab):  # glowna funkcja dopisujaca oraz sumujaca
+    long = len(tab)
+    counter = 0  # licznik liczb pierwszych w danej liscie
+    for i in range(long):  # pierwsza petla sprawdzajaca ilosc liczb pierwszych w tablicy
+        if prime(tab[i]):
+            counter += 1
+    long += counter
+    for i in range(long):  # druga petla dodajaca 0 po kazdej liczbie pierwszej
+        a = tab[i]
+        if prime(a):  # moduł .insert (miejsce w tablicy, wartosc) dodaje 0
+            tab.insert(i + 1, 0)
+        else:
+            continue
+    print(tab)  # print listy nr 1
+    tab2 = []  # druga tablica ktora ma przechowywac sumy podzbiorów
+    sum = 0
+    for s in range(long):
+        if tab[s] != 0:  # warunek na wykrycie 0 w tablicy
+            sum += tab[s]
+        else:
+            # jesli nie bedzie 0 to dopisze wczesniej policzona sume do nowej tablicy
+            tab2.append(sum)
+            sum = 0  # wyzerowanie "licznika" sumy aby mozna bylo liczyc kolejny podzbior
+    print(tab2)  # print listy nr 2
+    if len(tab2) >= 2:  # warunek na dlugosc tablicy ktora ma byc sprawdzona nwd
+        first = tab2[0]
+        second = tab2[1]
+        wynik = nwd(first, second)
+        return print(f"NWD 3 pierwszych liczb to: {wynik}")
+
+
+if __name__ == '__main__':
+    # przypadki generowane poprzez losowanie liczb
+    # while True:
+    #     n = int(input("Podaj ile liczb mam wpisac w tablice: "))
+    #     tab = [random.randint(1, 100) for i in range(n)]
+    #     problem(tab)
+
+    # przypadki testowe do sprawdzenia recznego.
+    # tab = [7,45,45,34,53,45,4,3,11,18,30]
+    # tab = [63, 60, 53, 1, 93, 2, 15, 4, 17, 80]
+    # problem(tab)
+
+    ##### Przypadki testowe #####
+    # Przypadek nr 1:
+    # output: lista po dodaniu 0
+    # Przypadek nr 2:
+    # output: lista po dodaniu 0 [7,0,45,45,34,53,0,45,4,3,0,11,0,18,30]. Wynik: [7, 177, 52, 11]
+    # Przypadek nr 3:
+    # output: lista po dodaniu 0 [63, 60, 53, 0, 1, 93, 2, 0, 15, 4, 17, 0, 80] Wynik: [176, 96, 36]
+    # Przypadki kolejne:
+    # Program jako głowne zalozenie ma generowac liczby losowe i wpisywac je do tablicy, tak wiec reszta sprawdzen moze zostac wykonana na losowo dobranych tabliach:
+    # Przypadek z problemem:
+    # gdy n =1000000, progam ma problem.
