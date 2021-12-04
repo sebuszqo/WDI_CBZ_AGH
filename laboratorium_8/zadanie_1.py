30d79d0ce99a500d0218c1ab26096ac502e1a2e5
sebuszqo <sekn.kn@gmail.com>
2021-12-04 22:07:06 +0100

    Poprawki do lab 8

--- laboratorium_8/zadanie_1.py
+++ laboratorium_8/zadanie_1.py
@@ -1,7 +1,6 @@
 import random
 from typing import Counter
 
-
 def prime(a):  # funkcja sprawdzajaca czy liczba jest pierwsza
     if a == 2 or a == 3:
         return True
@@ -13,7 +12,6 @@ def prime(a):  # funkcja sprawdzajaca czy liczba jest pierwsza
             return False
         i += 6
     return True
-
 
 def nwd(first, second):  # funkcja szukajaca NWD z dwoch liczb
     if second == 0:
@@ -53,19 +51,24 @@ def problem(tab):  # glowna funkcja dopisujaca oraz sumujaca
         wynik = nwd(first, second)
         return print(f"NWD 2 pierwszych liczb to: {wynik}")
 
-
 if __name__ == '__main__':
     # przypadki generowane poprzez losowanie liczb
-    # while True:
-    # n = int(input("Podaj ile liczb mam wpisac w tablice: "))
-    # tab = [random.randint(1, 100) for i in range(n)]
-    # problem(tab)
+    while True:
+        try:
+            n = int(input("Podaj ile liczb mam wpisac w tablice: "))
+            if n<= 0:
+                raise ValueError
+            tab = [random.randint(1, 100) for i in range(n)]
+            problem(tab)
+        except ValueError:
+            print("Podałeś złą wartość n, spróbuj ponownie")
 
     # przypadki testowe do sprawdzenia recznego.
-    # tab = [7,45,45,34,53,45,4,3,11,18,30]
+    # tab = [7, 45, 45, 34, 53, 45, 4, 3, 11, 18, 30]
     # tab = [63, 60, 53, 1, 93, 2, 15, 4, 17, 80]
-    tab = [52, 95, 26, 13, 46, 39, 27, 45, 87, 80, 77, 77, 93, 17, 43]
-    problem(tab)
+    # tab = [52, 95, 26, 13, 46, 39, 27, 45, 87, 80, 77, 77, 93, 17, 43]
+    # problem(tab)
+
 
     ##### Przypadki testowe #####
     # Przypadek nr 1:
@@ -74,6 +77,8 @@ if __name__ == '__main__':
     # output: lista po dodaniu 0 [7,0,45,45,34,53,0,45,4,3,0,11,0,18,30]. Wynik: [7, 177, 52, 11]
     # Przypadek nr 3:
     # output: lista po dodaniu 0 [63, 60, 53, 0, 1, 93, 2, 0, 15, 4, 17, 0, 80] Wynik: [176, 96, 36]
+    # Przypadek nr 4: 
+    # output: list po dodaniu 0 [65, 3, 0, 17, 0, 83, 0, 44, 23, 0, 43, 0, 15, 82, 49]  Wynik: [68, 17, 83, 67, 43]
     # Przypadki kolejne:
     # Program jako głowne zalozenie ma generowac liczby losowe i wpisywac je do tablicy, tak wiec reszta sprawdzen moze zostac wykonana na losowo dobranych tabliach:
     # Przypadek z problemem:
---
+++ laboratorium_9/zad_10.py
@@ -0,0 +1,67 @@
+
+from random import randint
+def gen_tab(n):
+
+    tab = [[randint(1, 100) for _ in range(n)]for _ in range(n)]
+    # tab = [
+    #     [52, 68, 35, 9, 91, 1, 79, 86], 
+    #     [76, 43, 27, 62, 2, 64, 9, 47], 
+    #     [22, 83, 53, 17, 24, 27, 26, 36], 
+    #     [82, 78, 43, 46, 18, 38, 1, 58], 
+    #     [71, 22, 40, 15, 55, 94, 89, 26], 
+    #     [63, 93, 51, 18, 23, 59, 57, 27], 
+    #     [90, 24, 45, 79, 53, 29, 33, 80], 
+    #     [31, 35, 21, 86, 29, 32, 77, 24]
+    #     ]
+    print(tab)
+    
+    return tab
+
+def is_prime(n):
+    if n==2 or n == 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0 or n < 2:
+        return False
+    i = 5
+    while i*i <= n:
+        if n % i == 0 or n % (i+2) == 0:
+            return False
+        i += 6
+    return True
+def has_prime(num):
+
+    while num != 0:
+        if is_prime(num % 10):
+            return True
+        num //= 10
+    return False
+
+def problem_15(tab):
+    long  = len(tab)
+   
+    for i in range(long):
+        flag = False
+        for j in range(long):
+            if has_prime(tab[i][j]):
+                flag = True
+            else:
+                flag = False
+                break
+        
+        if flag:
+            return True, tab[i]
+    return False, "nie ma takiego wiersza"
+        
+if __name__ == "__main__":
+    try:
+        n = int(input("podaj wymiar n x n tablicy: "))
+        if n <= 0:
+            raise ValueError
+    except ValueError:
+        print("wprowadziłeś niepoprawne n, spróbuj ponownie:")
+    Tabliczka= gen_tab(n)
+    one, two = problem_15(Tabliczka)
+    print(one, two)
+
+
+   
